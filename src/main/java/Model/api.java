b52c52cb7f6227d04f76cbee49001760b188a47a
jorge <jorgelizarraga1994@gmail.com>
2025-04-29 20:49:44 -0300

    segundo commit

---
+++ src/main/java/Model/api.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class api {
+
+
+    public api() throws IOException {
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,42 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Que moneda quiere convertir?");
+        String moneda = scanner.nextLine();
+        // Setting URL
+        String url_str = "https://v6.exchangerate-api.com/v6/389926c0eaf191d18eeafe8c/latest/"+ moneda;
+        System.out.println("A que moneda?");
+        String convertirA = scanner.nextLine();
+
+// Making Request
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+// Convert to JSON
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+// Accessing object
+
+        JsonObject req_result = jsonobj.get("conversion_rates").getAsJsonObject();
+        int resultado = req_result.get(convertirA).getAsInt();
+        System.out.println(resultado);
+
+
+
+
+    }
 }
